ee582b9191242828a26795d9038bee02c0a57361
Wan Akashah <wanakashah14833@gmail.com>
2025-03-21 11:16:28 +0800

    Update

--- main/utility/generate_treev2.py
+++ main/utility/generate_treev2.py
@@ -194,11 +194,11 @@ def find_trunk(pcd, center_coord, h_list, h, ransac_results, ratio:float = None,
     if len(clouds) == 0:
         logging.info(f'No trunk found')
         ransac_results[f"n_cloud_{prim}_{dev_deg}"] = 0
-        ransac_results[f"n_cloud_center_{prim}_{dev_deg}"] = 0
-        ransac_results[f"n_cloud_ground_{prim}_{dev_deg}"] = 0
-        ransac_results[f"n_cloud_top_{prim}_{dev_deg}"] = 0
-        ransac_results[f"max_z_{prim}_{dev_deg}"] = 0
-        ransac_results[f"cloud_top_{prim}_{dev_deg}"] = []
+        # ransac_results[f"n_cloud_center_{prim}_{dev_deg}"] = 0
+        # ransac_results[f"n_cloud_ground_{prim}_{dev_deg}"] = 0
+        # ransac_results[f"n_cloud_top_{prim}_{dev_deg}"] = 0
+        # ransac_results[f"max_z_{prim}_{dev_deg}"] = 0
+        # ransac_results[f"cloud_top_{prim}_{dev_deg}"] = []
         return None, None, ransac_results
     
     # Filter the cloud based on the center coordinate and height
@@ -252,11 +252,11 @@ def find_trunk(pcd, center_coord, h_list, h, ransac_results, ratio:float = None,
 
     # Append results to the list
     ransac_results[f"n_cloud_{prim}_{dev_deg}"] = len(clouds)
-    ransac_results[f"n_cloud_center_{prim}_{dev_deg}"] = len(cloud_center)
-    ransac_results[f"n_cloud_ground_{prim}_{dev_deg}"] = len(cloud_ground)
-    ransac_results[f"n_cloud_top_{prim}_{dev_deg}"] = len(cloud_top)
-    ransac_results[f"max_z_{prim}_{dev_deg}"] = max_z
-    ransac_results[f"cloud_top_{prim}_{dev_deg}"] = cloud_top
+    # ransac_results[f"n_cloud_center_{prim}_{dev_deg}"] = len(cloud_center)
+    # ransac_results[f"n_cloud_ground_{prim}_{dev_deg}"] = len(cloud_ground)
+    # ransac_results[f"n_cloud_top_{prim}_{dev_deg}"] = len(cloud_top)
+    # ransac_results[f"max_z_{prim}_{dev_deg}"] = max_z
+    # ransac_results[f"cloud_top_{prim}_{dev_deg}"] = cloud_top
 
     return meshes, filtered_clouds, ransac_results
     
@@ -370,6 +370,7 @@ class TreeGen():
                         
                         if clouds is None:
                             continue
+                    # break # Kasya: Remove this break to iterate through all the parameters
 
                 # Save results to a CSV file
                 # Define the header for the CSV file
