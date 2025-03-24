b15c6535dcd16daaeed5dfa8788adf2486375e08
Wan Akashah <wanakashah14833@gmail.com>
2025-03-24 18:20:39 +0800

    Update

--- main/utility/generate_treev2.py
+++ main/utility/generate_treev2.py
@@ -343,16 +343,16 @@ class TreeGen():
                 print(f"\nTree index: {index} h detected: {total_detected}")
                 
                 # Kasya: Find trunk using RANSAC
+                prim = int(596.11 * np.log(len(np.asarray(singular_tree.points))) - 5217.5)
+                print(f"prim: {prim}")
                 ransac_results = {
                     "n_points": len(np.asarray(singular_tree.points)),
                     "h_preds": h_list[0],
-                    "n_supp": 0,
+                    "n_supp": prim,
                     "n_gens": 0,
                     "h_gens": 0
                 }
                 # for prim in range(prim_min, prim_max, prim_step)
-                prim = int(596.11 * np.log(len(np.asarray(singular_tree.points))) - 5217.5)
-                print(f"prim: {prim}")
                 meshes, clouds, ransac_results = find_trunk(singular_tree, coord, h_list, h, ransac_results, prim=prim, dev_deg=deg)
                 results_df = pd.DataFrame([ransac_results])
                 results_df.to_csv(csv_file_path, index=False, mode='a', header=False)
