6013c722c7247a0d2a72e1f9efe3944e3f20c87c
Wan Akashah <wanakashah14833@gmail.com>
2025-03-27 00:49:12 +0800

    Update

--- main/utility/generate_treev2.py
+++ main/utility/generate_treev2.py
@@ -157,7 +157,195 @@ def regenerate_Tree(pcd, center_coord:tuple, radius_expand:int=5, zminmax:list=[
             if a is not None:
                 temp_tree+=a
     return temp_tree
+
+def ransac_gen_cylinders(pcd, prim:int = 500, dev_deg:int = 25, r_min:float = 0.4, r_max:float = 0.7):
+    """
+    Process the pcd using RANSAC to generate the trunk cylinder
+    Args:
+        pcd (open3d.PointCloud): Point cloud of the tree
+        prim (int, optional): Min N points for primitive. Defaults to 500.
+        dev_deg (int, optional): Max deviation of shape in degrees. Defaults to 25.
+        r_min (float, optional): Min radius of the cylinder. Defaults to 0.4.
+        r_max (float, optional): Max radius of the cylinder. Defaults to 0.7.
+    Returns:
+        meshes (list): List of meshes
+        clouds (list): List of clouds in ccPointCloud
+    """
+    # Convert open3d.PointCloud to ccPointCloud
+    points = np.asarray(pcd.points)
+    cloud = cc.ccPointCloud('cloud')
+    cloud.coordsFromNPArray_copy(points)
     
+    # RANSAC Parameters
+    ransac_params = cc.RANSAC_SD.RansacParams()
+
+    # RANSAC save leftover points (Default: true) //Do not change 'filter_cyl_center' use leftover points
+    ransac_params.createCloudFromLeftOverPoints = True
+    # RANSAC least square fitting (important for trunk cylinder, Default: true)
+    ransac_params.allowFitting = True
+    # RANSAC attempt to simplify shape (Default: true)
+    ransac_params.allowSimplification = True
+    # RANSAC set random color for each shape found (Default: true) 
+    ransac_params.randomColor = True
+
+    # Primitive shape to be detected
+    ransac_params.setPrimEnabled(cc.RANSAC_SD.RANSAC_PRIMITIVE_TYPES.RPT_CYLINDER,True)
+    ransac_params.setPrimEnabled(cc.RANSAC_SD.RANSAC_PRIMITIVE_TYPES.RPT_CONE,False)
+    ransac_params.setPrimEnabled(cc.RANSAC_SD.RANSAC_PRIMITIVE_TYPES.RPT_PLANE,False)
+    ransac_params.setPrimEnabled(cc.RANSAC_SD.RANSAC_PRIMITIVE_TYPES.RPT_SPHERE,False)
+    ransac_params.setPrimEnabled(cc.RANSAC_SD.RANSAC_PRIMITIVE_TYPES.RPT_TORUS,False)
+
+    # RANSAC min N primitive points (Default: 500)
+    ransac_params.supportPoints = prim
+
+    # RANSAC max deviation of shape (Default: 25 degrees)
+    ransac_params.maxNormalDev_deg = dev_deg
+
+    # RANSAC cylinder parameters (Default: inf, inf) 
+    # Oil Palm trunk dia 45-65 cm (https://bioresources.cnr.ncsu.edu/resources/the-potential-of-oil-palm-trunk-biomass-as-an-alternative-source-for-compressed-wood/)
+    ransac_params.minCylinderRadius = r_min
+    ransac_params.maxCylinderRadius = r_max
+
+    # RANSAC calculate
+    ransac_params.optimizeForCloud(cloud)
+    meshes, clouds = cc.RANSAC_SD.computeRANSAC_SD(cloud,ransac_params)
+
+    if len(clouds) == 0:
+        print("No trunk found")
+        return None, None
+    
+    return meshes, clouds
+
+def filter_cyl_center(trunk_ccpcds, center_coord:tuple, x_max:float, y_max:float, center_tol:float = 0.7):
+    """
+    Filter for center clouds except the last one (the last is the leftover)
+    Args:
+        trunk_ccpcds (list): List of ccPointClouds
+        center_coord (tuple): Center coordinate of the tree
+        x_max (float): Max x coordinate of the tree
+        y_max (float): Max y coordinate of the tree
+        center_tol (float, optional): Tolerance for the center coordinate. Defaults to 0.7.
+    Returns:
+        dict: Filtered center clouds
+        dict: Generated center coordinates
+    """
+    filtered_centers = {}
+    gens_ctr = {}
+    for index, trunk_ccpcd in enumerate(trunk_ccpcds[:-1]):
+        trunk_np = trunk_ccpcd.toNpArray()
+
+        # Use mean to find the center cluster of the cloud (tried with min/max but not good, center not detected)
+        trunk_x_center = trunk_np[:,0].mean()
+        trunk_y_center = abs(trunk_np[:,1].mean())
+
+        x_tol = center_coord[0]-center_tol < trunk_x_center < center_coord[0]+center_tol
+        y_tol = center_coord[1]-center_tol < trunk_y_center < center_coord[1]+center_tol
+        if x_tol and y_tol:
+            trunk_x_center_m = x_max - trunk_x_center
+            trunk_y_center_m = y_max - trunk_y_center
+            gens_ctr[index] = (trunk_x_center_m, trunk_y_center_m)
+            filtered_centers[index] = trunk_ccpcd
+    filtered_centers[index+1] = trunk_ccpcds[-1]
+
+    if len(gens_ctr) == 0:
+        print("No center found")
+        return None, None
+    
+    return filtered_centers, gens_ctr
+    
+def filter_cyl_height(filtered_centers, h_ref:float, z_min:float, z_tol:float = 0.1, h_tol:int = 3):
+    """
+    Filter cylinder height 
+    Args:
+        filtered_centers (dict): Filtered center clouds in ccPointCloud
+        h_ref (float): Height reference of the tree
+        z_min (float): Ground coordinate of the pcd
+        z_tol (float, optional): Tolerance for the z coordinate. Defaults to 0.1.
+    Returns:
+        dict: Filtered height clouds
+        list: Generated index and height
+    """
+    filtered_h = {}
+    gens_h = []
+    for index, trunk_ccpcd in filtered_centers.items():
+        trunk_np = trunk_ccpcd.toNpArray()
+        trunk_z_min, trunk_z_max = trunk_np[:,2].min(), trunk_np[:,2].max()
+        z_tols = trunk_z_min-z_tol < z_min < trunk_z_max+z_tol
+        if z_tols:
+            filtered_h[index] = trunk_ccpcd 
+
+            trunk_h = trunk_z_max - trunk_z_min
+            h_tols = h_ref - h_tol < trunk_h < h_ref
+            if h_tols:
+                gens_h.append([index, trunk_h])
+
+    if len(gens_h) == 0:
+        print("No height found")
+        return None, None
+    
+    return filtered_h, gens_h
+
+def find_trunk2(pcd, center_coord:tuple, h_ref:float, center_tol:float = 0.7, z_tol:float = 0.1, h_tol:int = 3):
+    """
+    Find the trunk using the center of the tree via RANSAC
+    Algo:
+    - RANSAC pcd to find the trunk cylinder's pcds
+    - Iterate trunk cylinders
+        1. find pcd within the center coordinate
+        2. find pcd touching ground
+        3. find pcd with height within the range (h_tol) and provided height (h_ref)
+        4. save pcd information
+    Args:
+        pcd (open3d.PointCloud): Single tree pcd
+        center_coord (tuple): Center coordinate of the tree
+        h_ref (float): Height reference of the tree
+        center_tol (float, optional): Tolerance for the center coordinate. Defaults to 0.7.
+        z_tol (float, optional): Tolerance for the z coordinate. Defaults to 0.1.
+        h_tol (int, optional): Tolerance for the height. Defaults to 3.
+    Returns:
+        open3d.PointCloud: Trunk point cloud
+        float: Trunk height
+        float: Trunk diameter
+        float: Trunk volume
+        float: Trunk volume from crown calculation
+    """
+    # Primitive ratio based on the number of points
+    # prim = int(596.11 * np.log(len(np.asarray(pcd.points))) - 5217.5)
+    prim = int(0.01*len(np.asarray(pcd.points)))
+
+    # RANSAC pcd to find the trunk cylinder
+    trunk_meshes, trunk_ccpcds = ransac_gen_cylinders(pcd, prim=prim, deg=45) # 45 deg gave best result for trunk
+    
+    # Extract open3d point cloud to numpy array
+    points = np.asarray(pcd.points)
+    z_min = points[:,2].min()
+    x_min, x_max = points[:,0].min(), points[:,0].max()
+    y_min, y_max = abs(points[:,1].max()), abs(points[:,1].min())
+
+    filtered_centers, filtered_centers_m = filter_cyl_center(trunk_ccpcds, center_coord, x_max, y_max, center_tol)
+
+    filtered_heights, filtered_heights_m = filter_cyl_height(filtered_centers, h_ref, z_min, z_tol, h_tol)
+
+    # Get trunk diameter and volume
+    if filtered_heights_m is not None:
+        max_h_height = max(filtered_heights_m, key=lambda x: x[1])[1]
+        max_h_index = max(filtered_heights_m, key=lambda x: x[1])[0]
+
+        # Convert ccpcd to o3d pcd  
+        trunk_ccpcd_np = filtered_heights[max_h_index].toNpArray()
+        trunk_pcd = o3d.geometry.PointCloud()
+        trunk_pcd.points = o3d.utility.Vector3dVector(trunk_ccpcd_np)
+
+        # Get trunk diameter and volume
+        trunk_d = diameter_at_breastheight(trunk_pcd, ground_level=z_min)
+        trunk_v = np.pi * trunk_d * max_h_height
+        trunk_mesh, trunk_v_c = crown_to_mesh(trunk_pcd)
+        # show_mesh_cloud(trunk_mesh, trunk_pcd) // debug
+
+        return trunk_pcd, max_h_height, trunk_d, trunk_v, trunk_v_c
+    return None, None, None, None, None
+
+
 def find_trunk(pcd, center_coord, h_list, h, ransac_results, ratio:float = None, prim:int = 500, dev_deg:int = 25, r_min:float = 0.4, r_max:float = 0.7):
     """
     Find the trunk using the center of the tree via RANSAC
@@ -212,10 +400,7 @@ def find_trunk(pcd, center_coord, h_list, h, ransac_results, ratio:float = None,
     # RANSAC calculate
     ransac_params.optimizeForCloud(cloud)
     meshes, clouds = cc.RANSAC_SD.computeRANSAC_SD(cloud,ransac_params)
-    # if len(clouds) == 0:
-    #     print("Rerun")
-    #     ransac_params.supportPoints = prim - 100
-    #     meshes, clouds = cc.RANSAC_SD.computeRANSAC_SD(cloud,ransac_params)
+
     if len(clouds) == 0:
         print("No trunk found")
         return None, None, ransac_results, None, None, None, None
@@ -333,6 +518,40 @@ def find_trunk(pcd, center_coord, h_list, h, ransac_results, ratio:float = None,
         trunk_img_z = ccpcd2img(trunk_cloud_colored, axis='z', stepsize=stepsize)
 
     return meshes, filtered_h, ransac_results, combined_img_x, combined_img_z, trunk_img_x, trunk_img_z
+
+
+def find_crown2(pcd, trunk_pcd):
+    # Compute the trunk's bounding box
+    trunk_bbox = trunk_pcd.get_axis_aligned_bounding_box()
+
+    # Convert to numpy for easier processing
+    tree_points = np.asarray(pcd.points)
+
+    # Get min/max coordinates of the trunk's bounding box
+    min_bound = trunk_bbox.min_bound
+    max_bound = trunk_bbox.max_bound
+
+    # Create a mask to keep only points **outside** the trunk bounding box
+    mask = np.logical_or.reduce((
+        tree_points[:, 0] < min_bound[0], tree_points[:, 0] > max_bound[0],  # X-axis
+        tree_points[:, 1] < min_bound[1], tree_points[:, 1] > max_bound[1],  # Y-axis
+        tree_points[:, 2] < min_bound[2], tree_points[:, 2] > max_bound[2]   # Z-axis
+    ))
+
+    # Apply mask to get only the crown points
+    crown_points = tree_points[mask]
+
+    # Get crown diameter and height and volume
+    crown_pcd = o3d.geometry.PointCloud()
+    crown_pcd.points = o3d.utility.Vector3dVector(crown_points)
+    crown_d = crown_diameter(crown_pcd)
+    crown_mesh, crown_v = crown_to_mesh(crown_pcd)
+    # show_mesh_cloud(crown_mesh, crown_pcd)
+
+    if crown_d is None or crown_v is None:
+        return None, None, None
+
+    return crown_pcd, crown_d, crown_v
 
 def find_crown(pcd, clouds, ransac_results):
     trunk_h = max(ransac_results['h_gens'], key=lambda x: x[1])[1]
@@ -662,23 +881,15 @@ class TreeGen():
                 # Perform Operations
                 # new_coord = find_centroid_from_Trees(pcd,coord_list[0],3, [z_min, z_max])
                 singular_tree = regenerate_Tree(pcd, coord, 5, [z_min, z_max], h_incre=4)
-
-                # Kasya: Visualize the tree
-                # print(type(singular_tree)) # <class 'open3d.cuda.pybind.geometry.PointCloud'>
-                # o3d.visualization.draw_geometries([singular_tree])
-                print(f"\nTree index: {index} h detected: {total_detected}")
                 
                 # Kasya: Find trunk using RANSAC
                 # prim = int(596.11 * np.log(len(np.asarray(singular_tree.points))) - 5217.5)
                 prim = int(0.01*len(np.asarray(singular_tree.points)))
-                print(f"n_points: {len(np.asarray(singular_tree.points))}")
-                print(f"prim: {prim}")
                 ransac_results = {
+                    "index": index,
                     "n_points": len(np.asarray(singular_tree.points)),
                     "n_supp": prim,
-                    "n_gens": 0,
                     "h_preds": h_list[0],
-                    "h_gens": [],
                     "trunk_h": 0.0,
                     "trunk_d": 0.0,
                     "trunk_v": 0.0,
@@ -687,26 +898,38 @@ class TreeGen():
                     "crown_v": 0.0
                 }
                 # for prim in range(prim_min, prim_max, prim_step)
-                meshes, clouds, ransac_results, img_x, img_z, img_x_t, img_z_t = find_trunk(singular_tree, coord, h_list, h, ransac_results, prim=prim, dev_deg=45)
-                if ransac_results['trunk_h'] > 0:
-                    crown_pcd, crown_img = find_crown(singular_tree, clouds, ransac_results)
-                    cv2.imwrite(f"{ransac_daq_path}/crown_{index}.jpg", h_im_list[0])
-                    cv2.imwrite(f"{ransac_daq_path}/out_crown.jpg", crown_img)
-                    cc.SavePointCloud(crown_pcd, f"{ransac_daq_path}/crown_{index}.bin")
+                # meshes, clouds, ransac_results, img_x, img_z, img_x_t, img_z_t = find_trunk(singular_tree, coord, h_list, h, ransac_results, prim=prim, dev_deg=45)
+                trunk_pcd, trunk_h, trunk_d, trunk_v, trunk_v_c = find_trunk2(singular_tree, coord, h_list[0])
+                if trunk_pcd is not None:
+                    crown_pcd, crown_d, crown_v = find_crown2(singular_tree, trunk_pcd)
 
-                if img_x is not None or img_z is not None or img_x_t is not None or img_z_t is not None:
-                    # Save the images
-                    cv2.imwrite(f"{ransac_daq_path}/tree_x_{index}.jpg", img_x)
-                    cv2.imwrite(f"{ransac_daq_path}/tree_z_{index}.jpg", img_z)
-                    cv2.imwrite(f"{ransac_daq_path}/trunk_x_{index}.jpg", img_x_t)
-                    cv2.imwrite(f"{ransac_daq_path}/trunk_z_{index}.jpg", img_z_t)
-                    cv2.imwrite(f"{ransac_daq_path}/out_tree_x.jpg", img_x)
-                    cv2.imwrite(f"{ransac_daq_path}/out_tree_z.jpg", img_z)
-                    cv2.imwrite(f"{ransac_daq_path}/out_trunk.jpg", img_x_t)
+                if crown_pcd is not None:
+                    ransac_results['trunk_h'] = trunk_h
+                    ransac_results['trunk_d'] = trunk_d
+                    ransac_results['trunk_v'] = trunk_v
+                    ransac_results['trunk_v_c'] = trunk_v_c
+                    ransac_results['crown_d'] = crown_d
+                    ransac_results['crown_v'] = crown_v
 
-                    # Save the point clouds
-                    for k, v in clouds.items():
-                        cc.SavePointCloud(v, f"{ransac_daq_path}/trunk_{index}_{k}.bin")
+                # if ransac_results['trunk_h'] > 0:
+                #     crown_pcd, crown_img = find_crown(singular_tree, clouds, ransac_results)
+                #     cv2.imwrite(f"{ransac_daq_path}/crown_{index}.jpg", h_im_list[0])
+                #     cv2.imwrite(f"{ransac_daq_path}/out_crown.jpg", crown_img)
+                #     cc.SavePointCloud(crown_pcd, f"{ransac_daq_path}/crown_{index}.bin")
+
+                # if img_x is not None or img_z is not None or img_x_t is not None or img_z_t is not None:
+                #     # Save the images
+                #     cv2.imwrite(f"{ransac_daq_path}/tree_x_{index}.jpg", img_x)
+                #     cv2.imwrite(f"{ransac_daq_path}/tree_z_{index}.jpg", img_z)
+                #     cv2.imwrite(f"{ransac_daq_path}/trunk_x_{index}.jpg", img_x_t)
+                #     cv2.imwrite(f"{ransac_daq_path}/trunk_z_{index}.jpg", img_z_t)
+                #     cv2.imwrite(f"{ransac_daq_path}/out_tree_x.jpg", img_x)
+                #     cv2.imwrite(f"{ransac_daq_path}/out_tree_z.jpg", img_z)
+                #     cv2.imwrite(f"{ransac_daq_path}/out_trunk.jpg", img_x_t)
+
+                #     # Save the point clouds
+                #     for k, v in clouds.items():
+                #         cc.SavePointCloud(v, f"{ransac_daq_path}/trunk_{index}_{k}.bin")
 
                 results_df = pd.DataFrame([ransac_results])
                 results_df.to_csv(csv_file_path, index=False, mode='a', header=False)
