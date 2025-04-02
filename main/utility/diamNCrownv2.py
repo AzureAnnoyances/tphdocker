0ca903da2e851a926329e31c0e40043a2c4bebf9
chngdickson <chngdickson@gmail.com>
2025-04-02 19:49:07 +0800

    fwefwefwe

--- main/utility/diamNCrownv2.py
+++ main/utility/diamNCrownv2.py
@@ -63,7 +63,7 @@ def split_pcd_by2_with_height(pcd, z_ffb, z_grd, center_coord, expansion):
         min_xyz = [center_coord[0]-expansion[0]/2, -center_coord[1]-expansion[1]/2, crown.get_min_bound()[2]],
         max_xyz = [center_coord[0]+expansion[0]/2, -center_coord[1]+expansion[1]/2, crown.get_max_bound()[2]],
         axis='z', 
-        highest_first=True,
+        highest_first=False,
         depth_weighting=True  
     )
     print(filtered_trunk_pcd.shape)
