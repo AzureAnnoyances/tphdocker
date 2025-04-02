c635946d4cfdb3eddc2c65f718735c25afa3b319
chngdickson <chngdickson@gmail.com>
2025-04-02 19:48:38 +0800

    fewfwefwefwef

--- main/utility/diamNCrownv2.py
+++ main/utility/diamNCrownv2.py
@@ -31,7 +31,7 @@ def split_pcd_by2_with_height(pcd, z_ffb, z_grd, center_coord, expansion):
     tol = 1.0 # To Remove additional points, so the picture will have less ground or less leaves
     min_bound, max_bound  = pcd.get_min_bound(), pcd.get_max_bound()
     bbox_trunk = o3d.geometry.AxisAlignedBoundingBox(min_bound=(min_bound[0], min_bound[1], z_grd+tol), max_bound=(max_bound[0],max_bound[1], z_ffb-tol*2))
-    bbox_crown = o3d.geometry.AxisAlignedBoundingBox(min_bound=(min_bound[0], min_bound[1], z_ffb-tol*2), max_bound=(max_bound[0],max_bound[1], z_ffb+tol*2))
+    bbox_crown = o3d.geometry.AxisAlignedBoundingBox(min_bound=(min_bound[0], min_bound[1], z_ffb-tol*3), max_bound=(max_bound[0],max_bound[1], z_ffb+tol))
     trunk = pcd.crop(bbox_trunk)
     crown = pcd.crop(bbox_crown)
 
@@ -59,7 +59,7 @@ def split_pcd_by2_with_height(pcd, z_ffb, z_grd, center_coord, expansion):
     # Crown
     filtered_trunk_pcd, raster_image, raster_trunk_img = rasterize_3dto2D(
         pointcloud = np.array(crown.points), 
-        img_shape  = (1080,1080),
+        img_shape  = (640,640),
         min_xyz = [center_coord[0]-expansion[0]/2, -center_coord[1]-expansion[1]/2, crown.get_min_bound()[2]],
         max_xyz = [center_coord[0]+expansion[0]/2, -center_coord[1]+expansion[1]/2, crown.get_max_bound()[2]],
         axis='z', 
