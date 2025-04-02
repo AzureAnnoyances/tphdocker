765e751c2a0cbfa8748efd6335c47f827749aed0
chngdickson <chngdickson@gmail.com>
2025-04-02 18:11:03 +0800

    fwefwefwefw

--- main/utility/diamNCrownv2.py
+++ main/utility/diamNCrownv2.py
@@ -26,8 +26,8 @@ def split_pcd_by2_with_height(pcd, z_ffb, z_grd, center_coord, expansion):
     # o3d.visualization.draw_geometries([crown])
     
     # Trunk
-    min_xyz = [center_coord[0]-expansion[0]/2, center_coord[1]-expansion[1]/2, trunk.get_min_bound()[2]]
-    max_xyz = [center_coord[0]+expansion[0]/2, center_coord[1]+expansion[1]/2, trunk.get_max_bound()[2]]
+    min_xyz = [center_coord[0]-expansion[0]/2, -center_coord[1]-expansion[1]/2, trunk.get_min_bound()[2]]
+    max_xyz = [center_coord[0]+expansion[0]/2, -center_coord[1]+expansion[1]/2, trunk.get_max_bound()[2]]
     filtered_trunk_pcd, raster_image, raster_trunk_img = rasterize_3dto2D(
         pointcloud = np.array(trunk.points), 
         img_shape  = (640,640),
