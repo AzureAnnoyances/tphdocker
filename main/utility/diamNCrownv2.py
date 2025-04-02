3bcdc2f6e10d7d7c27a6ee1a6441029f6d4ed286
chngdickson <chngdickson@gmail.com>
2025-04-02 18:22:57 +0800

    fefwefwefwe

--- main/utility/diamNCrownv2.py
+++ main/utility/diamNCrownv2.py
@@ -15,7 +15,7 @@ def split_pcd_by2_with_height(pcd, z_ffb, z_grd, center_coord, expansion):
     center_coord: Tuple of (c_x, c_y, c_z) bounds.
     expansion: Tuple of (x, y) expansion.
     """ 
-    tol = 0.5 # To Remove additional points that welll ruins the picture
+    tol = 1.0 # To Remove additional points, so the picture will have less ground or less leaves
     min_bound, max_bound  = pcd.get_min_bound(), pcd.get_max_bound()
     bbox_trunk = o3d.geometry.AxisAlignedBoundingBox(min_bound=(min_bound[0], min_bound[1], z_grd+tol), max_bound=(max_bound[0],max_bound[1], z_ffb-tol))
     bbox_crown = o3d.geometry.AxisAlignedBoundingBox(min_bound=(min_bound[0], min_bound[1], z_ffb-tol), max_bound=max_bound)
@@ -34,7 +34,7 @@ def split_pcd_by2_with_height(pcd, z_ffb, z_grd, center_coord, expansion):
         max_xyz = [center_coord[0]+expansion[0]/2, -center_coord[1]+expansion[1]/2, trunk.get_max_bound()[2]],
         axis='z', 
         highest_first=True,
-        depth_weighting=True  
+        depth_weighting=False  
     )
     print(filtered_trunk_pcd.shape)
     cv2.imshow('trunk raster',raster_image)
@@ -49,7 +49,7 @@ def split_pcd_by2_with_height(pcd, z_ffb, z_grd, center_coord, expansion):
         max_xyz = [center_coord[0]+expansion[0]/2, -center_coord[1]+expansion[1]/2, crown.get_max_bound()[2]],
         axis='z', 
         highest_first=True,
-        depth_weighting=True  
+        depth_weighting=False  
     )
     print(filtered_trunk_pcd.shape)
     cv2.imshow('crown raster',raster_image)
