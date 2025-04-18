355ebc124a4968df8bbbedf15cf78b9b0ec1f7ce
chngdickson <chngdickson@gmail.com>
2025-04-18 18:01:43 +0800

    Test analysis of crown and stem

---
+++ main/utility/analysis.py
@@ -0,0 +1,201 @@
+import sys
+import math, random
+import numba
+import numpy as np
+from numba import jit
+sys.path.insert(1, '/root/sdp_tph/submodules/PCTM/pctm/src')
+
+import trimesh
+import pymeshfix
+from alphashape import alphashape
+from adTreeutils.misc.fitcyclinders import fit_vertical_cylinder_3D, fit_cylinders_to_stem
+      
+
+def make_circle(points):
+	# Convert to float and randomize order
+	shuffled = [(float(x), float(y)) for (x, y) in points]
+	random.shuffle(shuffled)
+	
+	# Progressively add points to circle or recompute circle
+	c = None
+	for (i, p) in enumerate(shuffled):
+		if c is None or not is_in_circle(c, p):
+			c = _make_circle_one_point(shuffled[ : i + 1], p)
+	return c
+
+
+# One boundary point known
+def _make_circle_one_point(points, p):
+	c = (p[0], p[1], 0.0)
+	for (i, q) in enumerate(points):
+		if not is_in_circle(c, q):
+			if c[2] == 0.0:
+				c = make_diameter(p, q)
+			else:
+				c = _make_circle_two_points(points[ : i + 1], p, q)
+	return c
+
+
+# Two boundary points known
+def _make_circle_two_points(points, p, q):
+	circ = make_diameter(p, q)
+	left  = None
+	right = None
+	px, py = p
+	qx, qy = q
+	
+	# For each point not in the two-point circle
+	for r in points:
+		if is_in_circle(circ, r):
+			continue
+		
+		# Form a circumcircle and classify it on left or right side
+		cross = _cross_product(px, py, qx, qy, r[0], r[1])
+		c = make_circumcircle(p, q, r)
+		if c is None:
+			continue
+		elif cross > 0.0 and (left is None or _cross_product(px, py, qx, qy, c[0], c[1]) > _cross_product(px, py, qx, qy, left[0], left[1])):
+			left = c
+		elif cross < 0.0 and (right is None or _cross_product(px, py, qx, qy, c[0], c[1]) < _cross_product(px, py, qx, qy, right[0], right[1])):
+			right = c
+	
+	# Select which circle to return
+	if left is None and right is None:
+		return circ
+	elif left is None:
+		return right
+	elif right is None:
+		return left
+	else:
+		return left if (left[2] <= right[2]) else right
+
+
+def make_diameter(a, b):
+	cx = (a[0] + b[0]) / 2
+	cy = (a[1] + b[1]) / 2
+	r0 = math.hypot(cx - a[0], cy - a[1])
+	r1 = math.hypot(cx - b[0], cy - b[1])
+	return (cx, cy, max(r0, r1))
+
+
+def make_circumcircle(a, b, c):
+	# Mathematical algorithm from Wikipedia: Circumscribed circle
+	ox = (min(a[0], b[0], c[0]) + max(a[0], b[0], c[0])) / 2
+	oy = (min(a[1], b[1], c[1]) + max(a[1], b[1], c[1])) / 2
+	ax = a[0] - ox;  ay = a[1] - oy
+	bx = b[0] - ox;  by = b[1] - oy
+	cx = c[0] - ox;  cy = c[1] - oy
+	d = (ax * (by - cy) + bx * (cy - ay) + cx * (ay - by)) * 2.0
+	if d == 0.0:
+		return None
+	x = ox + ((ax*ax + ay*ay) * (by - cy) + (bx*bx + by*by) * (cy - ay) + (cx*cx + cy*cy) * (ay - by)) / d
+	y = oy + ((ax*ax + ay*ay) * (cx - bx) + (bx*bx + by*by) * (ax - cx) + (cx*cx + cy*cy) * (bx - ax)) / d
+	ra = math.hypot(x - a[0], y - a[1])
+	rb = math.hypot(x - b[0], y - b[1])
+	rc = math.hypot(x - c[0], y - c[1])
+	return (x, y, max(ra, rb, rc))
+
+
+_MULTIPLICATIVE_EPSILON = 1 + 1e-14
+
+def is_in_circle(c, p):
+	return c is not None and math.hypot(p[0] - c[0], p[1] - c[1]) <= c[2] * _MULTIPLICATIVE_EPSILON
+
+
+# Returns twice the signed area of the triangle defined by (x0, y0), (x1, y1), (x2, y2).
+def _cross_product(x0, y0, x1, y1, x2, y2):
+	return (x1 - x0) * (y2 - y0) - (y1 - y0) * (x2 - x0)
+
+
+
+### --- All Analysis  --- ###
+def stem_crown_analysis(stem_cloud, crown_cloud):
+    stats = {}
+    stats.update(stem_analysis(stem_cloud, stats))
+    crown_mesh, crown_volume = crown_to_mesh(crown_cloud)
+    stats["crown_mesh"] = crown_mesh
+    stats["crown_volume"] = crown_volume
+    return stats
+
+### --- STEM ANALYSIS --- ###
+def stem_analysis(stem_cloud, stats:dict):
+    """Function to analyse tree crown o3d point cloud."""
+ 
+    # stem stats
+    start_point, end_point = stem_cloud.get_min_bound(), stem_cloud.get_max_bound()
+    ground_z = start_point[2]
+    breastheight = (end_point[2] - ground_z)/4
+    
+
+    # diameter at breastheight
+    dbh = diameter_at_breastheight(stem_cloud, ground_z, breastheight)
+    stats['DBH'] = dbh
+    stats['circumference_BH'] = dbh * np.pi
+
+    # stem analysis
+    cyl_array = fit_cylinders_to_stem(stem_cloud, .25)
+    stats['stem_CCI'] = (np.min(cyl_array[:,4]), np.max(cyl_array[:,4]))
+    stats['stem_mesh'], stats['stem_volume'] = crown_to_mesh(stem_cloud)
+    return stats
+
+@jit(nopython=True, cache=True)
+def axis_clip(points, axis, lower=-np.inf, upper=np.inf):
+    """
+    Clip all points within bounds of a certain axis.
+
+    Parameters
+    ----------
+    points : array of shape (n_points, 2)
+        The points.
+    axis : int
+        The axis to clip along.
+    lower : float (default: -inf)
+        Lower bound of the axis.
+    upper : float (default: inf)
+        Upperbound of the axis.
+
+    Returns
+    -------
+    A boolean mask with True entries for all points within the rectangle.
+    """
+    clip_mask = ((points[:, axis] <= upper) & (points[:, axis] >= lower))
+    return clip_mask
+
+def diameter_at_breastheight(stem_cloud, ground_level, breastheight):
+    """Function to estimate diameter at breastheight."""
+    try:
+        stem_points = np.asarray(stem_cloud.points)
+        z = ground_level + breastheight
+
+        # clip slice
+        mask = axis_clip(stem_points, 2, z-.15, z+.15)
+        stem_slice = stem_points[mask]
+        if len(stem_slice) < 20:
+            return None
+
+        # fit cylinder
+        radius = fit_vertical_cylinder_3D(stem_slice, .04)[2]
+
+        return 2*radius
+    except Exception as e:
+        print('Error at %s', 'tree_utils error', exc_info=e)
+        return None
+
+def crown_to_mesh(crown_cloud, method='alphashape', alpha=.8):
+	if method == 'alphashape':
+		try:
+			crown_cloud_sampled = crown_cloud.voxel_down_sample(0.4)
+			pts = np.asarray(crown_cloud_sampled.points)
+			mesh = alphashape(pts, alpha)
+			clean_points, clean_faces = pymeshfix.clean_from_arrays(mesh.vertices,  mesh.faces)
+			mesh = trimesh.base.Trimesh(clean_points, clean_faces)
+			mesh.fix_normals()
+			o3d_mesh = mesh.as_open3d
+		except Exception as e:
+			crown_cloud_sampled = crown_cloud.voxel_down_sample(0.2)
+			o3d_mesh, _ = crown_cloud_sampled.compute_convex_hull()
+	else:
+		crown_cloud_sampled = crown_cloud.voxel_down_sample(0.2)
+		o3d_mesh, _ = crown_cloud_sampled.compute_convex_hull()
+	o3d_mesh.compute_vertex_normals()
+	return o3d_mesh, o3d_mesh.get_volume()
--- main/utility/diamNCrownv2.py
+++ main/utility/diamNCrownv2.py
@@ -5,11 +5,14 @@ import open3d as o3d
 from scipy.spatial.transform import Rotation as R
 from scipy.optimize import leastsq
 import sys, pathlib
+
+# Personal Libs
 yolov7_main_pth = pathlib.Path(__file__).resolve().parent.parent.joinpath("yolov7")
 sys.path.insert(0, '/root/sdp_tph/submodules/proj_3d_and_2d')
 sys.path.insert(0, str(yolov7_main_pth))
 from raster_pcd2img import rasterize_3dto2D
 from segment.predict2 import Infer_seg
+from .analysis import stem_crown_analysis
 
 # Fix split tree to rasters
 # Fix split tree to crown
@@ -26,58 +29,6 @@ def display_inlier_outlier(cloud):
     o3d.visualization.draw_geometries([inlier_cloud, outlier_cloud])
     return inlier_cloud
     
-def split_pcd_by2_with_height(pcd, z_ffb, z_grd, center_coord, expansion):  
-    """
-    pcd: (N, 3) array of 3D points.
-    z_ffb: 
-    z_grd: (
-    center_coord: Tuple of (c_x, c_y, c_z) bounds.
-    expansion: Tuple of (x, y) expansion.
-    """ 
-    tol = 1.0 # To Remove additional points, so the picture will have less ground or less leaves
-    min_bound, max_bound  = pcd.get_min_bound(), pcd.get_max_bound()
-    bbox_trunk = o3d.geometry.AxisAlignedBoundingBox(min_bound=(min_bound[0], min_bound[1], z_grd+tol), max_bound=(max_bound[0],max_bound[1], z_ffb-tol*2))
-    bbox_crown = o3d.geometry.AxisAlignedBoundingBox(min_bound=(min_bound[0], min_bound[1], z_ffb-tol*3), max_bound=(max_bound[0],max_bound[1], z_ffb+tol))
-    bbox_crown_top = o3d.geometry.AxisAlignedBoundingBox(min_bound=(min_bound[0], min_bound[1], z_ffb+tol), max_bound=(max_bound[0],max_bound[1], z_ffb+tol*2))
-    trunk = pcd.crop(bbox_trunk)
-    crown = pcd.crop(bbox_crown)
-    crown_upper = pcd.crop(bbox_crown_top)
-
-    
-    # Trunk
-    filtered_trunk_pcd, raster_image, raster_trunk_img = rasterize_3dto2D(
-        pointcloud = np.array(trunk.points), 
-        img_shape  = (640,640),
-        min_xyz = (center_coord[0]-expansion[0]/2, -center_coord[1]-expansion[1]/2, trunk.get_min_bound()[2]),
-        max_xyz = (center_coord[0]+expansion[0]/2, -center_coord[1]+expansion[1]/2, trunk.get_max_bound()[2]),
-        axis='z', 
-        highest_first=False,
-        depth_weighting=True  
-    )
-
-    
-    # Crown
-    filtered_trunk_pcd, raster_image, raster_crown_img = rasterize_3dto2D(
-        pointcloud = np.array(crown.points), 
-        img_shape  = (640,640),
-        min_xyz = [center_coord[0]-expansion[0]/2, -center_coord[1]-expansion[1]/2, crown.get_min_bound()[2]],
-        max_xyz = [center_coord[0]+expansion[0]/2, -center_coord[1]+expansion[1]/2, crown.get_max_bound()[2]],
-        axis='z', 
-        highest_first=False,
-        depth_weighting=True  
-    )
-    
-    filtered_trunk_pcd, raster_image, raster_crown_upper_img = rasterize_3dto2D(
-        pointcloud = np.array(crown_upper.points), 
-        img_shape  = (640,640),
-        min_xyz = [center_coord[0]-expansion[0]/2, -center_coord[1]-expansion[1]/2, crown_upper.get_min_bound()[2]],
-        max_xyz = [center_coord[0]+expansion[0]/2, -center_coord[1]+expansion[1]/2, crown_upper.get_max_bound()[2]],
-        axis='z', 
-        highest_first=False,
-        depth_weighting=True  
-    )
-    return raster_trunk_img, raster_crown_img, raster_crown_upper_img
-
 
 class SingleTreeSegmentation():
     def __init__(self, weight_src):
@@ -98,25 +49,13 @@ class SingleTreeSegmentation():
             trunk_pcd, crown_pcd = self.split_Tree_to_trunkNCrown(
                 pcd, mask_crown=im_mask_crown, mask_trunk=im_mask_trunk)
             # o3d.visualization.draw_geometries([trunk_pcd, crown_pcd])
-            _, _, raster_trunk_img = rasterize_3dto2D(
-                pointcloud = np.array(trunk_pcd.points), 
-                img_shape  = (640,640),
-                axis='x',
-                highest_first=True,
-                depth_weighting=True 
-            )
-            _, _, raster_crown_img = rasterize_3dto2D(
-                pointcloud = np.array(crown_pcd.points), 
-                img_shape  = (640,640),
-                axis='x',
-                highest_first=True,
-                depth_weighting=True 
-            )
-            print(raster_crown_img.dtype, raster_trunk_img.dtype, raster_crown_img.shape, raster_trunk_img.shape)
-            return True, raster_crown_img, raster_trunk_img
+            single_tree_pcd = trunk_pcd+crown_pcd
+            tree_img = self.o3dpcd2img(single_tree_pcd, 640, 480)
+            stats = stem_crown_analysis(stem_cloud=trunk_pcd, crown_cloud=crown_pcd)
+            return True, tree_img
         else:
             # Dont do anything
-            return False, 0, 0
+            return False, 0
         
     def one_ch_to_3ch(self, single_channel):
         three_channel = np.stack([single_channel] * 3, axis=-1)
@@ -256,82 +195,24 @@ class SingleTreeSegmentation():
         crown_pcd.paint_uniform_color([1.0, 0.0, 0.0])
         
         return trunk_pcd, crown_pcd
-        
-    def calculate_diam_n_crown(self, pcd_trunk, pcd_crown):
-        pass
-# Algorithm of akasha
-
-# def find_trunk(pcd, center_coord:tuple, h_ref:float, center_tol:float = 0.7, z_tol:float = 0.1, h_tol:int = 3):
-#     """
-#     Find the trunk using the center of the tree via RANSAC
-#     Algo:
-#     - RANSAC pcd to find the trunk cylinder's pcds
-#     - Iterate trunk cylinders
-#         1. find pcd within the center coordinate
-#         2. find pcd touching ground
-#         3. find pcd with height within the range (h_tol) and provided height (h_ref)
-#         4. save pcd information
-#     Args:
-#         pcd (open3d.PointCloud): Single tree pcd
-#         center_coord (tuple): Center coordinate of the tree
-#         h_ref (float): Height reference of the tree
-#         center_tol (float, optional): Tolerance for the center coordinate. Defaults to 0.7.
-#         z_tol (float, optional): Tolerance for the z coordinate. Defaults to 0.1.
-#         h_tol (int, optional): Tolerance for the height. Defaults to 3.
-#     Returns:
-#         open3d.PointCloud: Trunk point cloud
-#         float: Trunk height
-#         float: Trunk diameter
-#         float: Trunk volume using cylinder calculation
-#         float: Trunk volume from crown calculation
-#     """
-#     # Primitive ratio based on the number of points
-#     # prim = int(596.11 * np.log(len(np.asarray(pcd.points))) - 5217.5)
-#     prim = int(0.01*len(np.asarray(pcd.points)))
-
-#     # RANSAC pcd to find the trunk cylinder
-#     trunk_meshes, trunk_ccpcds = ransac_gen_cylinders(pcd, prim=prim, dev_deg=45) # 45 deg gave best result for trunk
+            
     
-#     if trunk_ccpcds is None:
-#         return None, None, None, None, None
-
-#     # Extract open3d point cloud to numpy array
-#     points = np.asarray(pcd.points)
-#     z_min = points[:,2].min()
-#     x_min, x_max = points[:,0].min(), points[:,0].max()
-#     y_min, y_max = abs(points[:,1].max()), abs(points[:,1].min())
-
-#     # TODO: Test without filtering the center
-#     # filtered_heights_ccpd, filtered_heights_m = filter_cyl_height(trunk_ccpcds, h_ref, z_min, z_tol, h_tol)
-
-#     # TODO: Test with filtering the center
-#     filtered_centers_ccpd, filtered_centers_m = filter_cyl_center(trunk_ccpcds, center_coord, x_max, y_max, center_tol)
-
-#     if filtered_centers_ccpd is None:
-#         return None, None, None, None, None
-    
-#     filtered_heights_ccpd, filtered_heights_m = filter_cyl_height(filtered_centers_ccpd, h_ref, z_min, z_tol, h_tol)
-#     # end of filtering
-
-#     # Get trunk diameter and volume
-#     if filtered_heights_m is not None:
-#         max_h_height = max(filtered_heights_m, key=lambda x: x[1])[1]
-#         max_h_index = max(filtered_heights_m, key=lambda x: x[1])[0]
-
-#         # Convert ccpcd to o3d pcd  
-#         trunk_ccpcd_np = filtered_heights_ccpd[max_h_index].toNpArray()
-#         trunk_pcd = o3d.geometry.PointCloud()
-#         trunk_pcd.points = o3d.utility.Vector3dVector(trunk_ccpcd_np)
-
-#         # Get trunk diameter and volume
-#         trunk_d = diameter_at_breastheight(trunk_pcd, ground_level=z_min)
-#         trunk_mesh, trunk_v_c = crown_to_mesh(trunk_pcd, 'hull')
-#         # show_mesh_cloud(trunk_mesh, trunk_pcd) // debug
-
-#         if trunk_d is None or trunk_v_c is None:
-#             return None, None, None, None, None
-        
-#         trunk_v = np.pi * trunk_d * max_h_height
-
-#         return trunk_pcd, max_h_height, trunk_d, trunk_v, trunk_v_c
-#     return None, None, None, None, None
+    def o3dpcd2img(self, pcd, width, height, return_camera=False):
+        vis = o3d.visualization.Visualizer()
+        vis.create_window(width=width, height=height, visible=False)
+        vis.get_render_option().point_size = 2
+        vis.add_geometry(pcd)
+        view_ctl = vis.get_view_control()
+        view_ctl.set_zoom(0.6)
+        view_ctl.set_lookat(pcd.get_center())
+        view_ctl.set_up((1, 0, 0))  # set the positive direction of the x-axis as the up direction
+        view_ctl.set_front((0, 0, 1))  # set the positive direction of the x-axis toward you
+        vis.update_renderer()
+        img = np.array(vis.capture_screen_float_buffer(True))
+        # depth = np.array(vis.capture_depth_float_buffer(True))
+        # if return_camera:
+        #     # https://www.open3d.org/html/python_api/open3d.camera.PinholeCameraIntrinsic.html
+        #     cam = view_ctl.convert_to_pinhole_camera_parameters()
+        #     return img, depth, mask, cam.intrinsic.intrinsic_matrix, cam.extrinsic
+        # vis.destroy_window()
+        return img
