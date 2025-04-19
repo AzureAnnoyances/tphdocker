15c59abbc97a368d8caba7444a7b6b0c353ad4b3
chngdickson <chngdickson@gmail.com>
2025-04-19 15:52:38 +0800

    fefwefwef

--- main/utility/generate_treev2.py
+++ main/utility/generate_treev2.py
@@ -331,36 +331,36 @@ class TreeGen():
             for j, w in enumerate(w_loop):
                 min_x, max_x = w, w+x_increment+x_increment/4
                 min_y, max_y = h, h+y_increment+y_increment/4 
-                coords_x_bool = (coord[0] >= min_x) & (coord[0] <= max_x)
-                coords_y_bool = (coord[1] >= min_y) & (coord[1] <= max_y)
-                print(min_y, max_y, coord[1])
-                if coords_x_bool & coords_y_bool:
-                    almost_tree = get_tree_from_coord(pcd, 
-                                                      grd_pcd, 
-                                                      coord, 
-                                                      expand_x_y=[self.ex_w,self.ex_w], 
-                                                      expand_z=[z_min, z_max]
-                                                      )
-                    h, im , confi, z_grd, z_ffb, xy_ffb = get_h_from_each_tree_slice2(
-                        tree = almost_tree,
-                        model_short = self.obj_det_short,
-                        model_tall = self.obj_det_tall,
-                        img_size = self.side_view_img_size, 
-                        stepsize = self.side_view_step_size,
-                        img_dir = f"{self.sideViewOut}/{self.pcd_name}_{index}_{i}_{j}",
-                        gen_undetected_img = True,
-                        img_with_h = True,
-                        min_no_points = self.min_points_per_tree
-                        )
-                    
-                    # If detected
-                    if h > 0:
-                        rtn_dict["confi"].append(confi)
-                        rtn_dict["h"].append(h)
-                        rtn_dict["z_grd"].append(z_grd)
-                        rtn_dict["z_ffb"].append(z_ffb)
-                        rtn_dict["xy_ffb"].append(xy_ffb)
-                        rtn_dict["imgz"].append(im)
+                # coords_x_bool = (coord[0] >= min_x) & (coord[0] <= max_x)
+                # coords_y_bool = (coord[1] >= min_y) & (coord[1] <= max_y)
+                # print(min_y, max_y, coord[1])
+                # if coords_x_bool & coords_y_bool:
+                almost_tree = get_tree_from_coord(pcd, 
+                                                    grd_pcd, 
+                                                    coord, 
+                                                    expand_x_y=[self.ex_w,self.ex_w], 
+                                                    expand_z=[z_min, z_max]
+                                                    )
+                h, im , confi, z_grd, z_ffb, xy_ffb = get_h_from_each_tree_slice2(
+                    tree = almost_tree,
+                    model_short = self.obj_det_short,
+                    model_tall = self.obj_det_tall,
+                    img_size = self.side_view_img_size, 
+                    stepsize = self.side_view_step_size,
+                    img_dir = f"{self.sideViewOut}/{self.pcd_name}_{index}_{i}_{j}",
+                    gen_undetected_img = True,
+                    img_with_h = True,
+                    min_no_points = self.min_points_per_tree
+                    )
+                
+                # If detected
+                if h > 0:
+                    rtn_dict["confi"].append(confi)
+                    rtn_dict["h"].append(h)
+                    rtn_dict["z_grd"].append(z_grd)
+                    rtn_dict["z_ffb"].append(z_ffb)
+                    rtn_dict["xy_ffb"].append(xy_ffb)
+                    rtn_dict["imgz"].append(im)
                         
         if len(rtn_dict["h"]) <= 0:
             print("\nDetected")
