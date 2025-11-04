27e7965c2e7870d3044694eb728c50036fbde39a
chngdickson <chngdickson@gmail.com>
2025-11-04 21:53:50 +0800

    fwefwefwefwefwefwefwew

--- main/utility/diamNCrownv2.py
+++ main/utility/diamNCrownv2.py
@@ -83,7 +83,9 @@ class SingleTreeSegmentation():
         det_bbox, proto, n_det = self.model.forward(raster_trunk_img, conf_thres=0.20, iou_thres=0.45)
         if n_det > 0:
             print(f"N_det in trunk : {n_det}")
-            im_mask_trunk, n_valid_trunks, uv_center_trunk = self.model.im_mask_from_center_region(det_bbox, proto, cls=cls_idx, center_tol=center_tol)
+            # im_mask_trunk, n_valid_trunks, uv_center_trunk = self.model.im_mask_from_center_region(det_bbox, proto, cls=cls_idx, center_tol=center_tol)
+            im_mask_trunk = self.model.im_mask_from_cls(self, detection_bbox,im_mask, cls=2):
+            return True, im_mask_trunk
             if n_valid_trunks >0:
                 return True, im_mask_trunk
             else:
